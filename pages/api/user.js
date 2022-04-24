300dc5d9adcea622b44c41d48614e18c8b0c3fab
Colby Hemond <hemond.colby@gmail.com>
2022-04-24 15:28:25 -0400

    add weight chart

--- pages/api/user.js
+++ pages/api/user.js
@@ -6,8 +6,6 @@ export default async function handler(req, res) {
   const client = await clientPromise;
   const db = client.db(process.env.MONGODB_DB);
   
-
-
   switch (req.method) {
     case "POST":
         try {
