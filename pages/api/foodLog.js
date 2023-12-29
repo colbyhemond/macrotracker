3c1bff0f510c506df06b5acdbdd9d9909344c1e0
Colby Hemond <hemond.colby@gmail.com>
2023-12-29 13:58:41 -0500

    update

--- pages/api/foodLog.js
+++ pages/api/foodLog.js
@@ -4,7 +4,7 @@ import { getAuth } from "@clerk/nextjs/server";
 
 
 export default async function handler(req, res) {
-  const { userId } = getAuth(req)
+  // const { userId } = getAuth(req)
 
   console.log('ℹ️ Request Received');
   const client = await clientPromise;
@@ -42,7 +42,7 @@ export default async function handler(req, res) {
               date: {
                 $eq: req.query.date
               },
-              user: userId
+              // user: userId
             }).toArray()
             console.log(foodLog);
             res.status(201).json(foodLog)
