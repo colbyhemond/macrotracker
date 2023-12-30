f4ade0b258d6732d2d8ef895397adf4e221405dc
Colby Hemond <hemond.colby@gmail.com>
2023-12-30 09:41:57 -0500

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
 import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn } from '@clerk/nextjs';
 import { useRouter } from 'next/router';
+import * as Fathom from 'fathom-client';
+import { useEffect } from 'react';
+
 
 const publicPages = [
   '/'
@@ -9,9 +12,32 @@ const publicPages = [
 function MyApp({ Component, pageProps }) {
 
   const { pathname } = useRouter();
+  const router = useRouter();
 
   // Check if the current route matches a public page
   const isPublicPage = publicPages.includes(pathname);
+
+  useEffect(() => {
+    // Initialize Fathom when the app loads
+    // Example: yourdomain.com
+    //  - Do not include https://
+    //  - This must be an exact match of your domain.
+    //  - If you're using www. for your domain, make sure you include that here.
+    Fathom.load('KCQELPCT', {
+      includedDomains: ['www.macrotracker.app'],
+    });
+
+    function onRouteChangeComplete() {
+      Fathom.trackPageview();
+    }
+    // Record a pageview when route changes
+    router.events.on('routeChangeComplete', onRouteChangeComplete);
+
+    // Unassign event listener
+    return () => {
+      router.events.off('routeChangeComplete', onRouteChangeComplete);
+    };
+  }, []);
 
   return (
     <ClerkProvider>
