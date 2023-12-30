492ede8529a07d5a6e8f2738a724c2c29c6827ec
Colby Hemond <hemond.colby@gmail.com>
2023-12-30 09:19:41 -0500

    updates

--- pages/index.js
+++ pages/index.js
@@ -22,9 +22,9 @@ export default function About () {
         </div>
         <div className='py-10 mt-10 m-auto prose'>
           <h1 className='text-3xl'>A minimalistic approach to tracking macronutrients.</h1>
-          <p>We shouldn't be tied to our phones when we should be focusing on ourselves. We've eliminated the all encompassing health app features and included only the necessary elements.</p>
+          <p>We should not be tied to our phones when we should be focusing on ourselves. We have eliminated the all encompassing health app features and included only the necessary elements.</p>
           <p>Using the widely used standard calculation for how many calories you burn a day at baseline, the Mifflin St. Jeor equation</p>
-          <p>Not focusing on overall calories consumed, but rather how many macro of each macronutrient you've consumed provides a guide for a better balanced diet throughout the day.</p>
+          <p>Not focusing on overall calories consumed, but rather how many macro of each macronutrient you have consumed provides a guide for a better balanced diet throughout the day.</p>
           <p>You can track your weight each day, week, month. And each time your weight influences your baseline calories, so that each day you can focus on goals appropriate to who you are today, not who you were yesterday.</p>
         </div>
           <div className='pt-10 pb-2 px-5 flex justify-between'>
