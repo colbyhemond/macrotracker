49c5e325df19c53c5cffeafa8c3ab93465643ba8
Colby Hemond <hemond.colby@gmail.com>
2023-12-30 09:16:29 -0500

    updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,6 @@
 import '../styles/globals.css'
 import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn } from '@clerk/nextjs';
 import { useRouter } from 'next/router';
-import { useEffect } from "react"
-import { notify } from '../lib/utils/notifications'
-
 
 const publicPages = [
   '/'
@@ -15,26 +12,6 @@ function MyApp({ Component, pageProps }) {
 
   // Check if the current route matches a public page
   const isPublicPage = publicPages.includes(pathname);
-
-  useEffect(() => {
-    window.OneSignal = window.OneSignal || [];
-    OneSignal.push(function() {
-      OneSignal.init({
-        appId: "69a7f78d-c60a-400b-b92d-5ddaccbc6637",
-        safari_web_id: "web.onesignal.auto.58b504fd-a471-4836-bd65-020899577e4e",
-        notifyButton: {
-          enable: true,
-        },
-        allowLocalhostAsSecureOrigin: true,
-      });
-    });
-
-    // notify()
-
-    return () => {
-        window.OneSignal = undefined;
-    };
-  }, []); // <-- run this effect once on mount
 
   return (
     <ClerkProvider>
--- pages/index.js
+++ pages/index.js
@@ -6,20 +6,35 @@ export default function About () {
 
 
   return (<>
-      <div className='flex justify-center items-center py-10'>
-          <h1 className='text-3xl font-extrabold tracking-tight'>macrotracker.app</h1>
+      <div className='flex justify-center items-center py-10 bg-gradient-to-r from-indigo-950 via-indigo-800 to-indigo-950'>
+          <div className='text-3xl font-extrabold tracking-tight'>Macro Tracker</div>
       </div>
-      <div className='bg-base-300 h-screen p-10'>
-        <div className='flex justify-center gap-5'>
-          <button className='btn btn-primary' onClick={()=> router.push('/me')}>Sign In</button>
-          <button className='btn btn-secondary' onClick={()=> router.push('/me')}>Sign Up</button>
-        </div>
-      </div>
-      <div className='h-48 bg-base-200 p-10'>
-          <div>
-            {/* <div>Donate</div> */}
+      <div className='bg-base-300 bg-gradient-to-br from-indigo-950'>
+        <div className='flex justify-center gap-48 py-28'>
+          <div className='text-right'>
+            <div className='text-6xl font-extrabold tracking-tight'>Simply</div>
+            <div className='text-6xl font-extrabold tracking-tight'>Track</div>
+            <div className='text-6xl font-extrabold tracking-tight'>Macros</div>
           </div>
-          <div>Copyright 2024 Hemond Consulting LLC</div>
+          <div className='self-center'>
+            <button className='btn btn-lg btn-primary' onClick={()=> router.push('/app')}>Start Tracking</button>
+          </div>
+        </div>
+        <div className='py-10 mt-10 m-auto prose'>
+          <h1 className='text-3xl'>A minimalistic approach to tracking macronutrients.</h1>
+          <p>We shouldn't be tied to our phones when we should be focusing on ourselves. We've eliminated the all encompassing health app features and included only the necessary elements.</p>
+          <p>Using the widely used standard calculation for how many calories you burn a day at baseline, the Mifflin St. Jeor equation</p>
+          <p>Not focusing on overall calories consumed, but rather how many macro of each macronutrient you've consumed provides a guide for a better balanced diet throughout the day.</p>
+          <p>You can track your weight each day, week, month. And each time your weight influences your baseline calories, so that each day you can focus on goals appropriate to who you are today, not who you were yesterday.</p>
+        </div>
+          <div className='pt-10 pb-2 px-5 flex justify-between'>
+            {/* <div>Donate</div> */}
+            {/* <div>Submit Bug or Feature Request</div> */}
+          </div>
+          <div className='pb-2 px-5 text-center'>
+            <div className=''>Copyright 2024 Hemond Consulting LLC</div>
+          </div>
+          
       </div>
   </>)
 }
